749b7227571da48acecd2e69e844b75b968d8ae0
hhh <2234238825@qq.com>
2023-09-22 17:45:30 +0800

    finished config.h

--- include/config.h
+++ include/config.h
@@ -10,7 +10,8 @@
 #include "log.h"
 #include "util.h"
 using namespace std;
-namespace hhh {
+namespace hhh
+{
 
     class ConfiVarBase
     {
@@ -28,13 +29,16 @@ namespace hhh {
         std::string m_name;
         std::string  m_description;
     };
+
     template<class T>
-    class Configvar:public ConfiVarBase
+    class ConfigVar:public ConfiVarBase
     {
     public:
-        typedef std::shared_ptr<Configvar>ptr;
-        Configvar(const string&name,const T&default_value,const std::string&description="")
+        typedef std::shared_ptr<ConfigVar>ptr;
+
+        ConfigVar(const string&name,const T&default_value,const std::string&description="")
         : ConfiVarBase(name,description),m_val(default_value){}
+
         std::string toString()override
         {
             try
@@ -46,6 +50,7 @@ namespace hhh {
             }
             return "";
         }
+
         bool fromString(const std::string&val)override
         {
              try
@@ -56,8 +61,44 @@ namespace hhh {
                  
              }
         }
+
     private:
         T m_val;
+    };
+
+    class Config
+    {
+    public:
+        typedef std::map<std::string,ConfiVarBase::ptr> ConfigVarMap;
+
+        template<class T>
+        static typename ConfigVar<T>::ptr Lookup(const std::string&name,const T&default_value,const std::string description="")
+        {
+            auto tmp = Lookup<T>(name);
+            if(tmp)
+            {
+                HHH_LOG_INFO((HHH_LOG_ROOT()))<<"lookup name = "<<name<<    "exist";
+            }
+
+            if(name.find_first_not_of("abcdefjhliipfmvrqzlhduiw.123456789")!= std::string::npos)
+            {
+                HHH_LOG_ERROR(HHH_LOG_ROOT())<<"Lookup name invalid"<<name;
+            }
+
+            typename ConfigVar<T>::ptr v(new ConfigVar<T>(name,default_value,description));
+            s_datas[name] = v;
+        }
+
+        template<class T>
+        static typename ConfigVar<T>::ptr Lookup(const std::string &name)
+        {
+            auto it=s_datas.find(name);
+            if(it == s_datas.end())
+                return nullptr;
+            return std::dynamic_pointer_cast<ConfigVar<T> >(it->second);
+        }
+    private:
+        static ConfigVarMap s_datas;
     };
 
 } // hhh
